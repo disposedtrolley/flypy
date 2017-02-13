7672fbeaf54253d9bcde69d996ae89addf901422
James Liu <disposedtrolley@gmail.com>
2017-02-13 22:24:19 +1100

    moved query send functionality to query class

--- airfare_tracker.py
+++ airfare_tracker.py
@@ -1,24 +1,7 @@
-import requests
-import json
 from datetime import datetime
-from bs4 import BeautifulSoup
 from query import Query
 from query_response import QueryResponse
 from helper import convert_str_to_date
-
-
-def perform_search(query):
-    base_url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyABg87ZKo9OH5Xc7llvmbxBd8LlrZ0kiuM"  # NOQA
-    payload = query
-
-    r = requests.post(base_url, data=json.dumps(payload),
-                      headers={'Content-Type': 'application/json'})
-    r = r.text
-
-    text_file = open("data/test_data_multi_leg.json", "w")
-    text_file.write(r)
-    text_file.close()
-    return json.loads(r)
 
 
 def load_test_data():
@@ -46,11 +29,9 @@ def main():
                        None,
                        max_stops)
 
-    # print(test_query.format_query())
-
-    # data = perform_search(test_query.format_query())
-    data = load_test_data()
-    response = QueryResponse(data)
+    response = test_query.send()
+    # data = load_test_data()
+    # response = QueryResponse(data)
     trips = response.get_trips()
     for trip in trips:
         for journey in trip.journeys:
