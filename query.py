c03cb5b0c765292157dbb3f2aa5755586282dff6
James Liu <disposedtrolley@gmail.com>
2017-02-14 10:10:06 +1100

    added method stubs

--- query.py
+++ query.py
@@ -113,5 +113,126 @@ class Query:
         }
         return query
 
+    def add_origin(self, origin):
+        """Validates and adds an origin airport to the query.
+
+        Args:
+            origin (string): the IATA code of the originating airport.
+
+        Returns:
+            string: the name of the validated origin airport, or None if not
+                    found.
+        """
+
+    def add_dest(self, dest):
+        """Validates and adds a destination airport to the query.
+
+        Args:
+            origin (string): the IATA code of the destination airport.
+
+        Returns:
+            string: the name of the validated destination airport, or None
+                    if not found.
+        """
+
+    def add_dept_date(self, dept_date):
+        """Validates and adds a departure date to the query.
+
+        Args:
+            dept_date (datetime): a datetime object of the intended departure
+                                  date.
+
+        Returns:
+            string: the validated departure date in the format YYYY-MM-DD.
+        """
+
+    def add_return_date(self, return_date):
+        """Validates and adds a return date to the query. This is optional
+        and only specified if a return journey is requested.
+
+        Args:
+            dept_date (datetime): a datetime object of the intended return
+                                  date.
+
+        Returns:
+            string: the validated return date in the format YYYY-MM-DD.
+        """
+
+    def add_pax_adult(self, pax_adult):
+        """Validates and adds adult passengers to the query.
+
+        Args:
+            pax_adult (int): the number of adult passengers travelling.
+
+        Returns:
+            string: the validated adult passenger count.
+        """
+
+    def add_pax_child(self, pax_child):
+        """Validates and adds child passengers to the query.
+
+        Args:
+            pax_child (int): the number of child passengers travelling.
+
+        Returns:
+            string: the validated child passenger count.
+        """
+
+    def add_pax_senior(self, pax_senior):
+        """Validates and adds senior passengers to the query.
+
+        Args:
+            pax_senior (int): the number of senior passengers travelling.
+
+        Returns:
+            string: the validated senior passenger count.
+        """
+
+    def add_pax_infant_lap(self, pax_infant_lap):
+        """Validates and adds infant in lap passengers to the query.
+
+        Args:
+            pax_infant_lap (int): the number of infant in lap passengers
+            travelling.
+
+        Returns:
+            string: the validated infant in lap passenger count.
+        """
+
+    def add_pax_infant_seat(self, pax_infant_seat):
+        """Validates and adds infant in seat passengers to the query.
+
+        Args:
+            pax_infant_seat (int): the number of infant in seat passengers
+            travelling.
+
+        Returns:
+            string: the validated infant in seat passenger count.
+        """
+
+    def add_airline(self, airline):
+        """Adds a preferred airline to the query. Optional.
+
+        Args:
+            airline (string): the IATA code of the preferred airline.
+
+        Returns:
+            string: the validated airline name.
+        """
+
+    def add_max_stops(self, max_stops_dept, max_stops_return):
+        """Adds a restriction on the maximum layovers for the onward and return
+        journey.
+
+        Args:
+            max_stops_dept (int): the maximum allowed layovers for the
+                                  departing flight.
+            max_stops_return (int): the maximum allowed layovers for the
+                                    returning flight.
+
+        Returns:
+            string: the validated airline name.
+        """
+
     def _validate_iata(self):
         return None
