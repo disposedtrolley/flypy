58fbe0cb901dfaddbd7f7b937b3cf8d87ec6f98b
James Liu <disposedtrolley@gmail.com>
2017-02-14 18:49:49 +1100

    cleaned up imports

--- airfare_tracker.py
+++ airfare_tracker.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 from query import Query
 from query_response import QueryResponse
-from helper import convert_str_to_date, convert_str_to_date_tz_naive
-import datetime
+from helper import convert_str_to_date_tz_naive
+import json
 
 
 def load_test_data():
@@ -12,19 +11,21 @@ def load_test_data():
 
 
 def main():
-    # data = load_test_data()
-    # response = QueryResponse(data)
-    dept_date = convert_str_to_date_tz_naive("2017-07-09")
-    return_date = convert_str_to_date_tz_naive("2017-09-23")
-    query = Query()
-    print(query.add_origin("MEL"))
-    print(query.add_dest("PVG"))
-    print(query.add_dept_date(dept_date))
-    print(query.add_return_date(return_date))
-    print(query.add_pax(1))
-    print(query.add_airline("CA"))
-    print(query.add_max_stops(1, 1,))
-    response = query.send()
+    data = load_test_data()
+    response = QueryResponse(data)
+
+    # dept_date = convert_str_to_date_tz_naive("2017-07-09")
+    # return_date = convert_str_to_date_tz_naive("2017-09-23")
+    # query = Query()
+    # print(query.add_origin("MEL"))
+    # print(query.add_dest("PVG"))
+    # print(query.add_dept_date(dept_date))
+    # print(query.add_return_date(return_date))
+    # print(query.add_pax(1))
+    # print(query.add_airline("CA"))
+    # print(query.add_max_stops(1, 1))
+    # response = query.send()
+
     trips = response.get_trips()
     for trip in trips:
         for journey in trip.journeys:
